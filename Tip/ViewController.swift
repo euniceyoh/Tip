b44d683cf2cd92c3962ec53a9cc0f8d508b90184
euniceyoh <euniceohsj@gmail.com>
2020-07-22 19:07:46 -0700

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Eunice Oh on 7/22/20.
+//  Copyright © 2020 Eunice Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
