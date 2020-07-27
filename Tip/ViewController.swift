9ab09d0ededb1a80a81bb3568d99655878926dbf
euniceyoh <euniceohsj@gmail.com>
2020-07-27 03:28:11 -0700

    Implemented required features and added functionality for light/dark appearance

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -1,20 +1,70 @@
-//
-//  ViewController.swift
-//  Tip
-//
-//  Created by Eunice Oh on 7/22/20.
-//  Copyright © 2020 Eunice Oh. All rights reserved.
-//
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipAmountSegmentedControl: UISegmentedControl!
+    
+    var lightMode:Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+        switchTheme()
+        
+        tipPercentageLabel.text = String("$0.00");
+        totalLabel.text = String("$0.00")
+        
+    }
+//    struct ColorManager {
+//        static let paleYellow = Color("PaleYellow")
+//        static let darkerYellow = Color("DarkerPaleYellow")
+//    }
+    
+    fileprivate func switchTheme() {
+        if lightMode {
+            view.backgroundColor = UIColor(named: "PaleYellow")
+            billAmountTextField.backgroundColor = UIColor(named: "PaleYellow")
+            tipAmountSegmentedControl.backgroundColor = UIColor(named: "PaleYellow")
+            tipAmountSegmentedControl.selectedSegmentTintColor = UIColor(named: "DarkerPaleYellow")
+            lineView.backgroundColor = UIColor(named: "DarkerPaleYellow")
+            
+        } else {
+            view.backgroundColor = .gray
+            billAmountTextField.backgroundColor = .gray
+            tipAmountSegmentedControl.backgroundColor = .gray
+            tipAmountSegmentedControl.selectedSegmentTintColor = .darkGray
+            lineView.backgroundColor = .darkGray
+        }
+    }
+    
+    @IBAction func switchMode(_ sender: UIBarButtonItem) {
+        lightMode.toggle()
+        switchTheme()
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+//        1. Get the initial bill amount and calculate the tips.
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipAmountSegmentedControl.selectedSegmentIndex]
 
+//        2. Calculate the total cost.
+        let total = bill + tip
+
+//        3. Update the tip and total labels.
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
